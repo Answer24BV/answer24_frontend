a0006f09a4cea29b2f83d436bffc035d4f6de1ff
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-07-26 19:31:28 +0100

    index...

--- components/homepage/index.tsx
+++ components/homepage/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { HeroSection } from './HeroSection'
 import { AudienceSection } from './AudienceSection'
 import { FeaturesSection } from './FeatureSection'
+import SectionDownloadApp from './DownloadApp'
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
             <HeroSection />
             <FeaturesSection />
             <AudienceSection />
+            <SectionDownloadApp />
         </div>
     )
 }
