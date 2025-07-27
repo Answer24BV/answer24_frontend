7c4fb145288e9414984aa24d46a045098ddb26f8
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-07-27 13:41:20 +0100

    fixed routing error

--- app/[locale]/dashboard/account/invoice/[transactionId]/page.tsx
+++ app/[locale]/dashboard/account/invoice/[transactionId]/page.tsx
@@ -2,22 +2,8 @@ import InvoiceCard from "@/components/Invoice/InvoiceCard";
 import React from "react";
 
 const Page = async (props: any) => {
-  console.log("=== INVOICE PAGE COMPONENT START ===");
-  console.log("Page props received:", /* @next-codemod-error 'props' is passed as an argument. Any asynchronous properties of 'props' must be awaited when accessed. */
-  props);
-  
   const params = await props.params;
-  console.log("Resolved params:", params);
-  console.log("Transaction ID from params:", params.transactionId);
-  
-  // Log any search params if they exist
-  if ((await props.searchParams)) {
-    console.log("Search params:", (await props.searchParams));
-  }
-  
-  console.log("Rendering InvoiceCard component with transactionId:", params.transactionId);
-  console.log("=== INVOICE PAGE COMPONENT END ===");
-  
+
   return <InvoiceCard transactionId={params.transactionId} />;
 };
 
