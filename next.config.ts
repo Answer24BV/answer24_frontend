f1010fbc3feaac8b1c460fa39ebb7a51a8fe818c
Devben <devben@Devbentech.local>
2025-07-28 13:45:30 +0100

    push to pro

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,34 @@
 
+// import { NextConfig } from 'next';
+// import createNextIntlPlugin from 'next-intl/plugin';
+// import withPWA from 'next-pwa';
+
+// const nextConfig: NextConfig = {
+//   // Add your Next.js config options here
+// };
+
+// const withNextIntl = createNextIntlPlugin();
+
+// export default withPWA({
+//   dest: 'public',
+//   register: true,
+//   skipWaiting: true,
+//   disable: process.env.NODE_ENV === 'development',
+// })(withNextIntl(nextConfig));
+
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import withPWA from 'next-pwa';
-
 const nextConfig: NextConfig = {
-  // Add your Next.js config options here
+  /* config options here */
 };
 
 const withNextIntl = createNextIntlPlugin();
-
-export default withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-})(withNextIntl(nextConfig));
+export default withNextIntl(
+  withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  })
+);
