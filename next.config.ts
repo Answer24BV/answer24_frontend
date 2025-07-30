1fc3027991c9b54609a83b048ab2f2a608698b81
Solomon Olatunji <aotoluwalope@gmail.com>
2025-07-30 09:51:19 +0100

    fixing cpanel statics

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,20 @@
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 import withPWA from "next-pwa";
+
 const nextConfig: NextConfig = {
   output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
+
 export default withNextIntl(
   withPWA({
+    ...nextConfig,
     dest: "public",
     register: true,
     skipWaiting: true,
