51b99861f51044acc9f769450b84b38bb0d738fa
Solomon Olatunji <aotoluwalope@gmail.com>
2025-07-30 08:55:53 +0100

    enable static exports

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,16 @@
-
-// import { NextConfig } from 'next';
-// import createNextIntlPlugin from 'next-intl/plugin';
-// import withPWA from 'next-pwa';
-
-// const nextConfig: NextConfig = {
-//   // Add your Next.js config options here
-// };
-
-// const withNextIntl = createNextIntlPlugin();
-
-// export default withPWA({
-//   dest: 'public',
-//   register: true,
-//   skipWaiting: true,
-//   disable: process.env.NODE_ENV === 'development',
-// })(withNextIntl(nextConfig));
-
-import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
-import withPWA from 'next-pwa';
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+import withPWA from "next-pwa";
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
 };
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(
   withPWA({
-    dest: 'public',
+    dest: "public",
     register: true,
     skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
+    disable: process.env.NODE_ENV === "development",
   })
 );
