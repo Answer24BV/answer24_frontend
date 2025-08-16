202c1cac1a13f78a52af65206bc3bb5f72e06c84
Jaycode01 <josephlamidi01@gmail.com>
2025-08-16 03:22:18 +0100

    i tried cracking dow the task 13 but ithink i still need the backend dev wo built it
    And again i am done working on task 14

--- app/[locale]/blog/[blogId]/page.tsx
+++ app/[locale]/blog/[blogId]/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-import React, { use, useEffect, useState } from 'react';
-import { notFound, useParams } from 'next/navigation';
-import BlogDetails from '@/components/blog/BlogDetails';
-import { getBlogBySlug } from '@/app/[locale]/actions/blog-utils';
-import { Blog } from '@/types/blog.d';
+"use client";
+import React, { use, useEffect, useState } from "react";
+import { notFound, useParams } from "next/navigation";
+import BlogDetails from "@/components/blog/BlogDetails";
+import { getBlogBySlug } from "@/app/[locale]/actions/blog-utils";
+import { Blog } from "@/types/blog.d";
 
 export default function BlogPostPage() {
   const [post, setPost] = useState<Blog | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-  const { blogId } = useParams<{ blogId: string }>()
+  const { blogId } = useParams<{ blogId: string }>();
   useEffect(() => {
     const fetchBlog = async () => {
       setIsLoading(true);
@@ -18,7 +18,7 @@ export default function BlogPostPage() {
         const blogPost = await getBlogBySlug(blogId);
         setPost(blogPost.data);
       } catch (err) {
-        setError('Failed to fetch blog post.');
+        setError("Failed to fetch blog post.");
       } finally {
         setIsLoading(false);
       }
@@ -34,10 +34,7 @@ export default function BlogPostPage() {
 
   return (
     <main className="container mx-auto px-4 py-12 md:py-16 lg:py-20">
-      <BlogDetails 
-        post={post} 
-        relatedPosts={[]} 
-      />
+      <BlogDetails post={post} relatedPosts={[]} />
     </main>
   );
 }
