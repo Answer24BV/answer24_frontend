3c1b635601b9bb11baa888731dccca72371520e0
Solomon Olatunji <aotoluwalope@gmail.com>
2025-08-29 11:12:43 +0100

    updates

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,17 @@
-// next.config.js
-import { NextConfig } from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
-import withPWA from 'next-pwa';
+import { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+import withPWA from "next-pwa";
 
 // next.config.ts
 const nextConfig = {
   images: {
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'avatar.iran.liara.run',
-    //     pathname: '/public/**',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'images.tech.co',
-    //   },
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'res.cloudinary.com',
-    //   },
-    // ],
-    remotePatterns: [new URL('https://res.cloudinary.com/dt5lofhwv/image/upload/v1754478315/answer24_blogs/**')],
+    remotePatterns: [
+      new URL(
+        "https://res.cloudinary.com/dt5lofhwv/image/upload/v1754478315/answer24_blogs/**"
+      ),
+    ],
   },
-  
+
   output: "export",
 };
 
